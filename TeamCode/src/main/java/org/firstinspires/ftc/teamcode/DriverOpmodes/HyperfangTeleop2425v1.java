35034328fb78beaa39ff28e101286c6673daac8e
S-h-a-r-v-i-l <sarwatesharvil@gmail.com>
2024-09-28 14:21:21 -0500

    Create HyperfangTeleop2425v1.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverOpmodes/HyperfangTeleop2425v1.java
@@ -0,0 +1,99 @@
+package org.firstinspires.ftc.teamcode.DriverOpmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+@TeleOp
+public class HyperfangTeleop2425v1 extends LinearOpMode{
+    public DcMotorEx leftFront = null;
+    public DcMotorEx leftBack = null;
+    public DcMotorEx rightFront = null;
+    public DcMotorEx rightBack = null;
+    public CRServo rotServo = null;
+
+
+    @Override
+    public void runOpMode() {
+        double speed, lift; speed = 1; lift = 1;
+
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
+        rotServo = hardwareMap.get(CRServo.class, "rotServo");
+
+        leftBack.setDirection(DcMotorEx.Direction.REVERSE);
+        rightFront.setDirection(DcMotorEx.Direction.REVERSE);
+
+        leftFront.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        leftBack.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        rightFront.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        rightBack.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        waitForStart();
+
+        Gamepad currentGamepad1 = new Gamepad();
+        Gamepad currentGamepad2 = new Gamepad();
+
+        Gamepad previousGamepad1 = new Gamepad();
+        Gamepad previousGamepad2 = new Gamepad();
+
+        while (opModeIsActive()) {
+
+            previousGamepad1.copy(currentGamepad1);
+            previousGamepad2.copy(currentGamepad2);
+            currentGamepad1.copy(gamepad1);
+            currentGamepad2.copy(gamepad2);
+
+            double y = -currentGamepad1.left_stick_y * speed; // Remember, Y stick value is reversed
+            double x = currentGamepad1.left_stick_x * speed; // Counteract imperfect strafing
+            double rx = currentGamepad1.right_stick_x * speed;
+
+
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            leftFront.setPower(frontLeftPower);
+            leftBack.setPower(backLeftPower);
+            rightFront.setPower(frontRightPower);
+            rightBack.setPower(backRightPower);
+
+            if(gamepad1.b){
+                leftFront.setPower(1);
+                rightBack.setPower(1);
+                sleep(1000);
+            }
+
+            if(gamepad2.x){
+                rotServo.setPower(1);
+                sleep(200);
+            }
+
+            if(gamepad2.a){
+                rotServo.setPower(-1);
+                sleep(200);
+            }
+
+            if (currentGamepad1.right_bumper && !previousGamepad1.right_bumper) {speed = Math.min(speed + 0.2, 1);}
+            else if (currentGamepad1.left_bumper && !previousGamepad1.left_bumper) {speed = Math.max(speed - 0.2, 0);}
+
+            telemetry.addLine("speed: " + speed);
+            telemetry.update();
+            sleep(50);
+        }
+    }
+}
+
