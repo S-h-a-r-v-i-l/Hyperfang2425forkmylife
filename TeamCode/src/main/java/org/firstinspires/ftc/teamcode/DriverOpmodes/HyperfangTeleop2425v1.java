d7d63bba4f4e1081d13ff7369f6f76429bfa6191
S-h-a-r-v-i-l <sarwatesharvil@gmail.com>
2024-11-01 21:04:16 -0500

    Finished Teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverOpmodes/HyperfangTeleop2425v1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverOpmodes/HyperfangTeleop2425v1.java
@@ -107,42 +107,35 @@ public class HyperfangTeleop2425v1 extends LinearOpMode{
             lL.setPower(gamepad2.left_stick_y * 0.5);
             rL.setPower(gamepad2.left_stick_y * 0.5);
 
-            if(gamepad2.right_stick_button){
-                lRP.setPower(-0.1);
-                rRP.setPower(-0.1);
+            lRP.setPower(0.7 * gamepad2.right_stick_y);
+            rRP.setPower(-0.7 * gamepad2.right_stick_y);
+
+            if (gamepad2.right_bumper) {
+                clawOpen.setPower(0.5);
+            } else if (gamepad2.left_bumper) {
+                clawOpen.setPower(-0.5);
+            }else {
+                clawOpen.setPower(0);
             }
-            else {
-                lRP.setPower(0);
-                rRP.setPower(0);
+
+            if (gamepad2.right_trigger >= 0.5) {
+                clawBelt.setPower(0.5);
+            } else if (gamepad2.left_trigger >= 0.5) {
+                clawBelt.setPower(-0.5);
+            }else {
+                clawBelt.setPower(0);
             }
 
-//            if (gamepad2.right_bumper) {
-//                clawOpen.setPower(0.5);
-//            } else if (gamepad2.left_bumper) {
-//                clawOpen.setPower(-0.5);
-//            }else {
-//                clawOpen.setPower(0);
-//            }
-
-//            if (gamepad2.right_trigger >= 0.5) {
-//                clawBelt.setPower(0.5);
-//            } else if (gamepad2.left_trigger >= 0.5) {
-//                clawBelt.setPower(-0.5);
-//            }else {
-//                clawBelt.setPower(0);
-//            }
-
-//            if (gamepad2.y) {
-//                clawLeft.setPower(0.3);
-//                clawRight.setPower(0.3);
-//            } else if (gamepad2.a) {
-//                clawLeft.setPower(-0.3);
-//                clawRight.setPower(-0.3);
-//            } else {
-//                clawLeft.setPower(0);
-//                clawRight.setPower(0);
-//
-//            }
+            if (gamepad2.y) {
+                clawLeft.setPower(1);
+                clawRight.setPower(-1);
+            } else if (gamepad2.a) {
+                clawLeft.setPower(-1);
+                clawRight.setPower(1);
+            } else {
+                clawLeft.setPower(0);
+                clawRight.setPower(0);
+            }
 
 
             if (currentGamepad1.right_bumper && !previousGamepad1.right_bumper) {speed = Math.min(speed + 0.2, 1);}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/blueBaskets.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/blueBaskets.java
@@ -1,4 +1,64 @@
 package org.firstinspires.ftc.teamcode.autos;
 
-public class blueBaskets {
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous
+public class blueBaskets extends LinearOpMode {
+
+    public DcMotorEx leftFront = null;
+    public DcMotorEx leftBack = null;
+    public DcMotorEx rightFront = null;
+    public DcMotorEx rightBack = null;
+    public DcMotorEx lL = null;
+    public DcMotorEx rL = null;
+
+    public CRServo rRP = null;
+    public CRServo lRP = null;
+    public CRServo clawLeft = null; //hanging claws
+    public CRServo clawRight = null;
+    public CRServo clawOpen = null; //actual claw itself
+    public CRServo clawBelt = null; //rotational servo
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        ElapsedTime runtime = new ElapsedTime();
+
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
+        lL = hardwareMap.get(DcMotorEx.class, "leftLift");
+        rL = hardwareMap.get(DcMotorEx.class, "rightLift");
+        rRP = hardwareMap.get(CRServo.class, "rightRandP");
+        lRP = hardwareMap.get(CRServo.class, "leftRandP");
+        clawOpen = hardwareMap.get(CRServo.class, "clawOpen");
+        clawBelt = hardwareMap.get(CRServo.class, "clawBelt");
+        clawLeft = hardwareMap.get(CRServo.class, "clawRight");
+        clawRight = hardwareMap.get(CRServo.class, "clawLeft");
+
+        while(runtime.seconds() < 0.7){
+            leftFront.setPower(1);
+            rightFront.setPower(1);
+            leftBack.setPower(1);
+            rightBack.setPower(1);
+        }
+        while(runtime.seconds() < 1.7)
+            leftFront.setPower(1);
+            rightFront.setPower(1);
+            leftBack.setPower(1);
+            rightBack.setPower(1);
+        }
+
+    }
+
+
+
+
+
 }
