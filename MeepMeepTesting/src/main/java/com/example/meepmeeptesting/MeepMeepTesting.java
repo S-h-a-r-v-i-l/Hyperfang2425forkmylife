e0a7364d8bf927c38b4ca5d9660e5624c7e3abc6
Y-i-m-i-n-g <yiming.peng.yp@gmail.com>
2025-01-11 15:18:31 -0600

    Tested on MeepMeep + Ascent Zone Park Auto (untested)
    Just played around with odom, no testing bc robot is being rewired

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -15,12 +15,17 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-11.8, -61.7, Math.toRadians(90)))
-                .setTangent(Math.toRadians(45))
-                .splineToLinearHeading(new Pose2d(-57, -53, (5*Math.PI)/4), Math.toRadians(15))
-                .setTangent(Math.toRadians(155))
-                .splineToLinearHeading(new Pose2d(60, -60, (11*Math.PI)/6), Math.toRadians(205))
-                .build());
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(11.8, 61.7, Math.toRadians(180)))
+
+                .setTangent(Math.toRadians(330))
+                .splineToLinearHeading(new Pose2d(57, 53, (5*Math.PI)/4), Math.toRadians(0))
+                        .setTangent(Math.toRadians(250))
+                        .splineToLinearHeading(new Pose2d(26, 0, Math.PI), Math.toRadians(200))
+                        .build());
+
+//                .setTangent(Math.toRadians(155))
+//                .splineToLinearHeading(new Pose2d(60, -60, (11*Math.PI)/6), Math.toRadians(205))
+//                .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_OFFICIAL)
                 .setDarkMode(true)
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/BlueAscentPark.java
@@ -0,0 +1,190 @@
+package org.firstinspires.ftc.teamcode.autos;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+@Config
+@Autonomous(name = "BlueAscentPark", group = "Autonomous")
+public class BlueAscentPark extends LinearOpMode {
+    public class Lifts{
+        private DcMotorEx leftLift;
+        private DcMotorEx rightLift;
+
+        public Lifts(HardwareMap hardwareMap) {
+            leftLift = hardwareMap.get(DcMotorEx.class, "leftLift");
+            leftLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            leftLift.setDirection(DcMotorSimple.Direction.FORWARD);
+
+            rightLift = hardwareMap.get(DcMotorEx.class, "rightLift");
+            rightLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            rightLift.setDirection(DcMotorSimple.Direction.FORWARD);
+        }
+
+        public class LiftUp implements Action {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    leftLift.setPower(-0.8);
+                    rightLift.setPower(0.8);
+                    initialized = true;
+                }
+
+                double pos = leftLift.getCurrentPosition();
+                packet.put("liftPos", pos);
+                if (pos < 10) {
+                    return true;
+                } else {
+                    leftLift.setPower(0);
+                    rightLift.setPower(0);
+                    return false;
+                }
+            }
+        }
+        public Action liftUp() {
+            return new LiftUp();
+        }
+
+        public class LiftDown implements Action {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    leftLift.setPower(0.8);
+                    rightLift.setPower(-0.8);
+                    initialized = true;
+                }
+
+                double pos = leftLift.getCurrentPosition();
+                //Todo: might be wrong ngl
+                packet.put("liftPos", pos);
+                if (pos > 100.0) {
+                    return true;
+                } else {
+                    leftLift.setPower(0);
+                    rightLift.setPower(0);
+                    return false;
+                }
+            }
+        }
+        public Action liftDown(){
+            return new LiftDown();
+        }
+    }
+
+    public class Claw{
+        private CRServo clawOpen;
+        private CRServo asr;
+        private CRServo asl;
+
+        public Claw(HardwareMap hMap){
+            clawOpen = hMap.get(CRServo.class, "clawOpen");
+            asr = hMap.get(CRServo.class, "elbowR");
+            asl = hMap.get(CRServo.class, "elbowL");
+        }
+
+        public class CloseClaw implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                ElapsedTime t = new ElapsedTime();
+                t.startTime();
+                while(t.seconds() < 0.5){ clawOpen.setPower(-1);}
+                return false;
+            }
+        }
+        public Action closeClaw() {
+            return new CloseClaw();
+        }
+
+        public class OpenClaw implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                ElapsedTime t = new ElapsedTime();
+                t.startTime();
+                while(t.seconds() < 0.5){ clawOpen.setPower(1);}
+                return false;
+            }
+        }
+        public Action openClaw() {
+            return new OpenClaw();
+        }
+
+        public class RotateClawForward implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                ElapsedTime t = new ElapsedTime();
+                t.startTime();
+                while(t.seconds() < 0.3){ asl.setPower(-1); asl.setPower(1);}
+                return false;
+            }
+        }
+        public Action rotateClawForward() {
+            return new RotateClawForward();
+        }
+
+        public class RotateClawBackward implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                ElapsedTime t = new ElapsedTime();
+                t.startTime();
+                while(t.seconds() < 0.3){ asl.setPower(1); asl.setPower(-1);}
+                return false;
+            }
+        }
+        public Action rotateClawBackward() {
+            return new RotateClawBackward();
+        }
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Pose2d initialPose = new Pose2d(11.8, 61.7, Math.toRadians(180));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+        Claw claw = new Claw(hardwareMap);
+        Lifts lifts = new Lifts(hardwareMap);
+
+        TrajectoryActionBuilder toBasketsTAB = drive.actionBuilder(initialPose)
+                .setTangent(Math.toRadians(330))
+                .splineToLinearHeading(new Pose2d(57, 53, (5*Math.PI)/4), Math.toRadians(0));
+
+
+        Action basketCloseOut = toBasketsTAB.endTrajectory().fresh()
+                .setTangent(Math.toRadians(250))
+                .splineToLinearHeading(new Pose2d(26, 0, Math.PI), Math.toRadians(200))
+                .build()
+                ;
+
+        waitForStart();
+
+        Action toBasketsA = toBasketsTAB.build();
+
+        if(opModeIsActive()) {
+            Actions.runBlocking(
+                    new SequentialAction(
+                            toBasketsA,
+                            basketCloseOut
+                    )
+            );
+        }
+    }
+
+}
